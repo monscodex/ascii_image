db18c5d7b814815079de5b60a723c40ce5defdc7
monscodex <r2bs@protonmail.com>
2022-05-30 17:27:06 +0200

    Fixing options and adding --fontratio

--- ascii_image.py
+++ ascii_image.py
@@ -4,19 +4,19 @@ from os import get_terminal_size
 # Font ratio between the width over the hight (x/y)
 FONT_RATIO = 0.5
 
-def get_ascii_image(path, color, palette_code):
+def get_ascii_image(path, color, palette, fontratio):
     img = Image.open(path)
 
     # Calculate image's dimensions in order to fit in the terminal without loosing its ratio
-    width, height = get_displaying_dimensions(img)
+    width, height = get_displaying_dimensions(img, fontratio)
     img = img.resize((width, height))
 
-    ascii_representation = get_ascii_representation(img, palette_code, color)
+    ascii_representation = get_ascii_representation(img, palette, color)
 
     return ascii_representation
 
-def get_ascii_representation(img, palette_code, color):
-    palette = get_palette_from_code(palette_code)
+def get_ascii_representation(img, palette_option, color):
+    palette = get_palette_from_option(palette_option)
 
     pixels = img.load()
     width, height = img.size
@@ -38,10 +38,15 @@ def get_ascii_representation(img, palette_code, color):
 
     return text_image
 
-def get_palette_from_code(palette_code):
-    match palette_code:
+def get_palette_from_option(palette_option):
+    if palette_option['specified'] != None:
+        return palette_option['specified']
+    
+    match palette_option['code']:
         case 'expanded':
             return """ÆÑÊŒØMÉËÈÃÂWQBÅæ#NÁþEÄÀHKRŽœXgÐêqÛŠÕÔA€ßpmãâG¶øðé8ÚÜ$ëdÙýèÓÞÖåÿÒb¥FDñáZPäšÇàhû§ÝkŸ®S9žUTe6µOyxÎ¾f4õ5ôú&aü™2ùçw©Y£0VÍL±3ÏÌóC@nöòs¢u‰½¼‡zJƒ%¤Itocîrjv1lí=ïì<>i7†[¿?×}*{+()/»«•¬|!¡÷¦¯—^ª„”“~³º²–°­¹‹›;:’‘‚’˜ˆ¸…·¨´` """
+        case 'standard':
+            return """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """
         case 'reduced':
             return "@%#*+=-:. "
         case 'block':
@@ -57,8 +62,8 @@ def get_corresponding_palette_char(pixel, palette):
 
     return palette[palette_index]
 
-def get_displaying_dimensions(img):
-    image_dimensions = get_image_dimensions(img)
+def get_displaying_dimensions(img, fontratio):
+    image_dimensions = get_image_dimensions(img, fontratio)
     terminal_dimensions = get_terminal_size()
 
     ratios = [
@@ -77,11 +82,11 @@ def get_displaying_dimensions(img):
     # Else
     return supposed_image_width, terminal_dimensions[1]
 
-def get_image_dimensions(img):
+def get_image_dimensions(img, fontratio):
     width, height = img.size
 
     # Compensate for the taller font
-    height *= FONT_RATIO
+    height *= fontratio
     height = int(height)
 
     return (width, height)
--- cli.py
+++ cli.py
@@ -7,20 +7,51 @@ from ascii_image import get_ascii_image
 
 app = typer.Typer()
 
-class Palette(str, Enum):
+class PaletteCode(str, Enum):
     expanded = "expanded"
+    standard = "standard"
     reduced = "reduced"
     block = "block"
 
+
+
+# Fontratio option
+def font_ratio_callback(value: float):
+    if value < 0:
+        raise typer.BadParameter("Negative fontratios are not possible")
+    elif value == 0:
+        raise typer.BadParameter("Null fontratios are not possible")
+    
+    return value
+
+fontratio_option = typer.Option(0.5, callback=font_ratio_callback, help="The proportion between the width of the font to the height of the font. (x/y)")
+
+
 @app.command()
-def print_image(path : str, color : bool = typer.Option(True), palette_code : Palette = "expanded"):
-    ascii_image = get_ascii_image(path, color, palette_code)
+def print_image(
+        path : str,
+        color : bool = typer.Option(True),
+        palette_code : PaletteCode = "standard",
+        palette : str = None,
+        fontratio: float = fontratio_option
+    ):
+    palette = {'code': palette_code, 'specified': palette}
+
+    ascii_image = get_ascii_image(path, color, palette, fontratio)
 
     print(ascii_image)
 
 @app.command()
-def convert_image(path : str, output_file_path : str, palette_code : Palette = "expanded"):
-    ascii_image = get_ascii_image(path, color=False, palette_code=palette_code)
+def convert_image(
+        path : str,
+        output_file_path : str,
+        palette_code : PaletteCode = "standard",
+        palette : str = None,
+        fontratio: float = fontratio_option
+    ):
+    palette = {'code': palette_code, 'specified': palette}
+
+    ascii_image = get_ascii_image(path, color=False, palette=palette, fontratio=fontratio)
 
     with open(output_file_path, 'w') as f:
         f.write(ascii_image)
